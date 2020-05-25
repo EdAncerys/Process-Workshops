f04c9d45be665aafed9f345b5da572aa3bbba3df
Ed <lookatemail@gmail.com>
2020-05-25 16:46:33 +0100

    able to return different verse every time a method called

--- ten-green-bottle/lib/bottle.rb
+++ ten-green-bottle/lib/bottle.rb
@@ -1,13 +1,30 @@
 
 class TenGreenBottles
-  attr_accessor :count_bottles
+  attr_accessor :count_bottles, :numbers_in_words
 
   def initialize
-    @count_bottles = 0
+    @count_bottles = 10
+    @numbers_in_words = {
+      10 => 'Ten',
+      9 => 'Nine',
+      8 => 'Eight',
+      7 => 'Seven',
+      6 => 'Six'
+    }
   end
 
   def verses
-    @count_bottles += 1
+    verse = "
+    #{@numbers_in_words[@count_bottles]} green bottles
+    Hanging on the wall
+    #{@numbers_in_words[@count_bottles]} green bottles
+    Hanging on the wall
+    And if one green bottle
+    Should accidentally fall
+    There'll be #{(@numbers_in_words[@count_bottles - 1]).downcase} green bottles
+    Hanging on the wall"
+    @count_bottles -= 1
+    p verse
   end
 
 end
--- ten-green-bottle/spec/bottle_spec.rb
+++ ten-green-bottle/spec/bottle_spec.rb
@@ -3,12 +3,25 @@ require 'bottle'
 describe TenGreenBottles do
 
   it 'able to return count for a bottles' do
-    expect(subject.count_bottles).to eq 0
+    expect(subject.count_bottles).to eq 10
   end
 
-  it 'should be able to increment a count by 1' do
+  it 'should be able to decrement a count by 1' do
     4.times { subject.verses }
-    expect(subject.count_bottles).to eq 4
+    expect(subject.count_bottles).to eq 6
+  end
+
+  it 'should be able to return verses by decrementing count' do
+    3.times { subject.verses }
+    expect(subject.verses).to eq "
+    Seven green bottles
+    Hanging on the wall
+    Seven green bottles
+    Hanging on the wall
+    And if one green bottle
+    Should accidentally fall
+    There'll be six green bottles
+    Hanging on the wall"
   end
 
 end
