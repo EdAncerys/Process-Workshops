22732f75e8ad80050a24dbb5c16783bd2ae67b8e
Ed <lookatemail@gmail.com>
2020-05-25 11:20:36 +0100

    refactor code

--- ten-minute-walk/lib/ten_minute.rb
+++ ten-minute-walk/lib/ten_minute.rb
@@ -1,8 +1,31 @@
 
 def ten_minute_walk?(walk)
-  if walk == ['w', 's'] || walk == ['w', 'w']
-    false
-  else
-    true
+
+  w = 0
+  e = 0
+  s = 0
+  n = 0
+
+  walk.each do |direction|
+    # if direction == 'w'
+    #   w += 1
+    # elsif direction == 'e'
+    #   e += 1
+    # elsif direction == 's'
+    #   s += 1
+    # elsif direction == 'n'
+    #   n += 1
+    # end
+    w += 1 if direction == 'w'
+    e += 1 if direction == 'e'
+    s += 1 if direction == 's'
+    n += 1 if direction == 'n'
   end
+
+  if w == e && s == n
+    true
+  else
+    false
+  end
+
 end
