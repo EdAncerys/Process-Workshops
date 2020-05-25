08645fb4d69b2df985e03b35a55e9deeed379ba9
Ed <lookatemail@gmail.com>
2020-05-25 12:27:57 +0100

    code refactor

--- scrabble/lib/scrabble.rb
+++ scrabble/lib/scrabble.rb
@@ -1,14 +1,26 @@
 
 class Scrabble
 
+  attr_reader :score_1, :score_2
+
+  # attr_accessor :score
+
+  def initialize
+    @score_1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']
+    @score_2 = ['D', 'G']
+
+  end
+
   def sum_count(word)
-    if word == 'A'
-      1
-    elsif word == 'DD'
-      4
-    else
-      2
+
+    to_array = word.split('')
+    score = 0
+    to_array.each do |letter|
+      score += 1 if score_1.include? letter
+      score += 2 if score_2.include? letter
     end
+
+    score
   end
 
 end
