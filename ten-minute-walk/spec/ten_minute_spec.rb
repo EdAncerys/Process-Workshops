cc26dc292e59cd55ff09a5d82542a43aeaa9d311
Ed <lookatemail@gmail.com>
2020-05-25 11:23:50 +0100

    able to return true if [w, s, e, e, n, n, e, s, w, w] passed

--- ten-minute-walk/spec/ten_minute_spec.rb
+++ ten-minute-walk/spec/ten_minute_spec.rb
@@ -1,21 +1,25 @@
-require 'ten_minute'
+require "ten_minute"
 
-describe 'Ten Minute Walk' do
+describe "Ten Minute Walk" do
 
-  context 'when false' do
+  context "when false" do
     it 'should be able to return false if ["w", "s"] passed in' do
-      expect(ten_minute_walk?(['w', 's'])).to eq false
+      expect(ten_minute_walk?(["w", "s"])).to eq false
     end
 
     it 'should be able to return false if ["w", "w"] passed in' do
-      expect(ten_minute_walk?(['w', 'w'])).to eq false
+      expect(ten_minute_walk?(["w", "w"])).to eq false
     end
 
   end
 
-  context 'when true' do
+  context "when true" do
     it 'should be able to return true if ["w", "e", "s", "n"] passed in' do
       expect(ten_minute_walk?(["w", "e", "s", "n"])).to eq true
+    end
+
+    it 'should be able return true if ["w", "s", "e", "e", "n", "n", "e", "s", "w", "w"] passed in' do
+      expect(ten_minute_walk?(["w", "s", "e", "e", "n", "n", "e", "s", "w", "w"])).to eq true
     end
 
   end
