be473638b81f013555b2e8c4711694223a2c5d50
Ed <lookatemail@gmail.com>
2020-05-25 17:16:19 +0100

    code refactor

--- checkout/lib/checkout.rb
+++ checkout/lib/checkout.rb
@@ -1,14 +1,16 @@
 
 class Checkout
 
-  def checkout_total(item)
-    if item == 'AA'
-      100
-    elsif item == 'A'
-      50
-    else
-      30
+  def checkout_total(list)
+    basket_total = 0
+    shopping_list = list.split('')
+
+    shopping_list.each do |item|
+      basket_total += 50 if item == 'A'
+      basket_total += 30 if item == 'B'
     end
+
+    basket_total
   end
 
 end
