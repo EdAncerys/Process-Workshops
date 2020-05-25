7e70f547fd7f8ff0acc2288c38ab763250792689
Ed <lookatemail@gmail.com>
2020-05-25 16:48:46 +0100

    refactor code

--- ten-green-bottle/lib/bottle.rb
+++ ten-green-bottle/lib/bottle.rb
@@ -9,7 +9,13 @@ class TenGreenBottles
       9 => 'Nine',
       8 => 'Eight',
       7 => 'Seven',
-      6 => 'Six'
+      6 => 'Six',
+      5 => 'Five',
+      4 => 'Four',
+      3 => 'Three',
+      2 => 'Two',
+      1 => 'One',
+      0 => 'no'
     }
   end
 
