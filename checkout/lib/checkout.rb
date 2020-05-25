c1e248c5e4d7036054d5035a3de2a64e4705914b
Ed <lookatemail@gmail.com>
2020-05-25 17:37:11 +0100

    able to apply discount if AAA passed in

--- checkout/lib/checkout.rb
+++ checkout/lib/checkout.rb
@@ -1,18 +1,36 @@
 
 class Checkout
+  attr_accessor :basket_total, :offer_A, :offer_B
+
+  def initialize
+    @basket_total = 0
+    @offer_A = 0
+    @offer_B = 0
+  end
 
   def checkout_total(list)
-    basket_total = 0
     shopping_list = list.split('')
 
     shopping_list.each do |item|
-      basket_total += 50 if item == 'A'
-      basket_total += 30 if item == 'B'
-      basket_total += 20 if item == 'C'
-      basket_total += 15 if item == 'D'
+      @basket_total += 50 if item == 'A'
+      @basket_total += 30 if item == 'B'
+      @basket_total += 20 if item == 'C'
+      @basket_total += 15 if item == 'D'
+      special_offers(item)
     end
 
     basket_total
+  end
+
+  private
+
+  def special_offers(item)
+    @offer_A += 1 if item == 'A'
+    @offer_B += 1 if item == 'B'
+    if @offer_A == 3
+      @basket_total -= 20
+      @offer_A -= 3
+    end
   end
 
 end
--- checkout/spec/checkout_spec.rb
+++ checkout/spec/checkout_spec.rb
@@ -2,28 +2,39 @@ require 'checkout'
 
 describe Checkout do
 
-  it 'should be able return 50 if A passed in' do
-    expect(subject.checkout_total('A')).to eq 50
+  context 'valid inputs withoud discount' do
+
+    it 'should be able return 50 if A passed in' do
+      expect(subject.checkout_total('A')).to eq 50
+    end
+
+    it 'should be able to return 100 if AA passed in' do
+      expect(subject.checkout_total('AA')).to eq 100
+    end
+
+    it 'shoud be able to return 30 if B passed in' do
+      expect(subject.checkout_total('B')).to eq 30
+    end
+
+    it 'should be able to return 60 if CCC passed in' do
+      expect(subject.checkout_total('CCC')).to eq 60
+    end
+
+    it 'shoud be able to return 30 if DD passed in' do
+      expect(subject.checkout_total('DD')).to eq 30
+    end
+
+    it 'should be able to return 230 if AABBCCDD passed in' do
+      expect(subject.checkout_total('AABBCCDD')).to eq 230
+    end
+
   end
 
-  it 'should be able to return 100 if AA passed in' do
-    expect(subject.checkout_total('AA')).to eq 100
-  end
+  context 'valid inputs with discount' do
 
-  it 'shoud be able to return 30 if B passed in' do
-    expect(subject.checkout_total('B')).to eq 30
-  end
-
-  it 'should be able to return 60 if CCC passed in' do
-    expect(subject.checkout_total('CCC')).to eq 60
-  end
-
-  it 'shoud be able to return 30 if DD passed in' do
-    expect(subject.checkout_total('DD')).to eq 30
-  end
-
-  it 'should be able to return 230 if AABBCCDD passed in' do
-    expect(subject.checkout_total('AABBCCDD')).to eq 230
+    it 'should be able to return 130 if AAA passed in' do
+      expect(subject.checkout_total('AAA')).to eq 130
+    end
   end
 
 end
